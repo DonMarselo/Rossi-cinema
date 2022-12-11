66c4ce281fbb3b1f2b1bedc46e3a5a6a5f3f4b82
DonMarselo <90642035+DonMarselo@users.noreply.github.com>
2022-12-11 23:40:51 +0100

    creazione file

---
+++ Lazzaretto Diego The Rossi Cinema/pagine/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../grafica/home.css"> 
+    <title>Home - The Rossi Cinema</title>
+</head>
+<body>
+    <div id="navbar">
+        <div><img src="../immagini/logo_rossi.jpg" id="logo_navbar"></div>
+        <div><h2>BENVENUTO NEL CINEMA ITIS ROSSI</h2></div>
+        <div><a href="login.php"><input type="button" id="login" onclick="" value="👤LOGIN"></a></div>
+    </div>
+</body>
+</html>
