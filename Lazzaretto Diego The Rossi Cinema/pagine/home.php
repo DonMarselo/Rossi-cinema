ad2c8d91402a81e0c2f81f6549359d931b512f90
DonMarselo <90642035+DonMarselo@users.noreply.github.com>
2022-12-12 11:47:46 +0100

    aggiunta spazio film

--- Lazzaretto Diego The Rossi Cinema/pagine/home.php
+++ Lazzaretto Diego The Rossi Cinema/pagine/home.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../immagini/icona_cinema.png">
     <link rel="stylesheet" href="../grafica/home.css"> 
+    <link rel="stylesheet" href="../grafica/film.css"> 
     <title>Home - The Rossi Cinema</title>
 </head>
 <body>
@@ -12,6 +14,16 @@
         <div><img src="../immagini/logo_rossi.jpg" id="logo_navbar"></div>
         <div><h2>BENVENUTO NEL CINEMA ITIS ROSSI</h2></div>
         <div><a href="login.php"><input type="button" id="login" onclick="" value="👤LOGIN"></a></div>
+    </div>
+
+    <div id="scelta_film">
+        <div id="starwars3">
+            sas
+        </div>
+
+        <div id="starwars2">
+            sas
+        </div>
     </div>
 </body>
 </html>
